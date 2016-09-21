134c0dccb7d3581ab9bb104c54f2aab4392907d4
Marek Siarkowicz <serathius@users.noreply.github.com>
2016-09-21 23:01:18 +0200

    Fix identation.

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"log"
 	"net/http"
 
-    "github.com/elazarl/goproxy"
+	"github.com/elazarl/goproxy"
 	"github.com/peterbourgon/srvproxy/proxy"
 	"github.com/peterbourgon/srvproxy/retry"
 )
@@ -19,26 +19,26 @@ func main() {
 	t := &http.Transport{}
 	t.RegisterProtocol("dnssrv", rt)
 
-    proxy := goproxy.NewProxyHttpServer()
-    proxy.Verbose = true
-    proxy.Tr = t
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	proxy.Tr = t
 
-    proxy.NonproxyHandler = http.HandlerFunc(
-        func(w http.ResponseWriter, r *http.Request) {
-		    if r.Host == "" {
-			    fmt.Fprintln(w, "Cannot handle requests without Host header, e.g., HTTP 1.0")
-			    return
-		    }
-		    r.URL.Scheme = "dnssrv"
-		    r.URL.Host = r.Host
-		    proxy.ServeHTTP(w, r)
+	proxy.NonproxyHandler = http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			if r.Host == "" {
+				fmt.Fprintln(w, "Cannot handle requests without Host header, e.g., HTTP 1.0")
+				return
+			}
+			r.URL.Scheme = "dnssrv"
+			r.URL.Host = r.Host
+			proxy.ServeHTTP(w, r)
 	})
 
-    proxy.OnRequest().DoFunc(
-        func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
-            r.URL.Scheme = "dnssrv"
-            return r,nil
-    })
-    log.Fatal(http.ListenAndServe(":1080", proxy))
+	proxy.OnRequest().DoFunc(
+		func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
+			r.URL.Scheme = "dnssrv"
+			return r,nil
+	})
+	log.Fatal(http.ListenAndServe(":1080", proxy))
 }
 
